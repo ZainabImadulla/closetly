4b86cb5b87cc265d57e6cd949abc4b2a66962388
ZainabImadulla <zainab.imadulla@icloud.com>
2024-08-09 20:01:49 +0530

    deleted some unecessary lines

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,5 @@
 import mysql from "mysql2"
-import dotenv from 'dotenv';
 
-
-dotenv.config();
-
-console.log(process.env.MYSQL_ROOT_PASSWORD);
 
 export const db =  mysql.createConnection({
     host : "localhost",
